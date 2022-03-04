a5a1e6c7c41718a6bc5b8813de954ce90b023f1a
linshan <1099013371@qq.com>
2022-03-04 18:11:30 +0800

    组件包优化

--- go.mod
+++ go.mod
@@ -7,15 +7,23 @@ require (
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator/v10 v10.9.0
+	github.com/go-redis/redis/v8 v8.11.4
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gorm.io/driver/mysql v1.3.2
+	gorm.io/gorm v1.23.2
 )
 
 require (
 	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
---
+++ pkg/database/gorm/db.go
@@ -0,0 +1,98 @@
+package gorm
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
+)
+
+type Options struct {
+	Dsn			string
+	Driver      string
+	DbName      string
+	Host        string
+	UserName    string
+	Password    string
+	Charset     string
+	Port        int
+	Prefix      string
+	MaxIdleConn int
+	MaxOpenConn int
+	MaxLifeTime time.Duration
+	ParseTime   string
+	Loc         string
+	OpenPlugin  bool
+}
+
+type PluginConfig struct {
+	Before func(gormDb *gorm.DB)
+	After  func(gormDb *gorm.DB, sql string, ts time.Time)
+}
+
+type DB struct {
+	*gorm.DB
+}
+
+func New(opts Options, pgc PluginConfig) (*DB, error) {
+	var (
+		dsn  string
+		err  error
+		conn *gorm.DB
+	)
+
+	if len(opts.Dsn) > 0 {
+		dsn = opts.Dsn
+	} else {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%s&loc=%s",
+			opts.UserName,
+			opts.Password,
+			opts.Host,
+			opts.Port,
+			opts.DbName,
+			opts.Charset,
+			opts.ParseTime,
+			opts.Loc)
+	}
+
+	conn, err = gorm.Open(
+		mysql.Open(dsn),
+		&gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix:   opts.Prefix, // 设置表前缀
+				SingularTable: true,         // 全局禁用表名复数
+			},
+		})
+
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDb, _ := conn.DB()
+	// 设置最大连接数 用于设置闲置的连接数.设置闲置的连接数则当开启的一个连接使用完成后可以放在池里等候下一次使用。
+	sqlDb.SetMaxIdleConns(opts.MaxIdleConn)
+	// 设置连接池 用于设置最大打开的连接数，默认值为0表示不限制.设置最大的连接数，可以避免并发太高导致连接mysql出现too many connections的错误。
+	sqlDb.SetMaxOpenConns(opts.MaxOpenConn)
+	// 设置最大连接超时
+	sqlDb.SetConnMaxLifetime(time.Minute * opts.MaxLifeTime)
+
+	if opts.OpenPlugin {
+		// 使用插件
+		_ = conn.Use(&TracePlugin{
+			Before: pgc.Before,
+			After:  pgc.After,
+		})
+	}
+
+	return &DB{conn}, nil
+}
+
+func (db *DB) Close() error {
+	sqlDB, err := db.DB.DB()
+	if err != nil {
+		return err
+	}
+
+	return sqlDB.Close()
+}
