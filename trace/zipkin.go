e6050da987f17eae6403d49f1d195063bcafbe0d
linshan <1099013371@qq.com>
2022-06-17 11:16:19 +0800

    新增 zipkin 实例参数

--- trace/zipkin.go
+++ trace/zipkin.go
@@ -8,9 +8,9 @@ type Zipkin struct {
 	exporter *exporterZipkin.Exporter
 }
 
-func NewZipkin(collectorURL string) (*Zipkin, error) {
+func NewZipkin(collectorURL string, opts ...exporterZipkin.Option) (*Zipkin, error) {
 	var zipkin = new(Zipkin)
-	exporter, err := exporterZipkin.New(collectorURL)
+	exporter, err := exporterZipkin.New(collectorURL, opts...)
 	if err != nil {
 		return nil, err
 	}
